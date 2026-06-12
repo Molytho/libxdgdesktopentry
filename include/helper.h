ed094f4f4c6c7b170bb9e4978d734d38f295d63e
Robin Ebert <ebertrobin2002@gmail.com>
2026-06-12 11:18:42 +0200

    Housekeeping

--- include/helper.h
+++ include/helper.h
@@ -1,20 +1,20 @@
-#ifndef LIB_XDGDESKTOPENTRYPP_HELPER_H
-#define LIB_XDGDESKTOPENTRYPP_HELPER_H
+#ifndef LIB_XDGPP_HELPER_H
+#define LIB_XDGPP_HELPER_H
 
 #if defined _WIN32 || defined __CYGWIN__
-# ifdef BUILDING_LIBXDGDESKTOPENTRY
+# ifdef BUILDING_LIBXDGPP
 #  define API_PUBLIC __declspec(dllexport)
 # else
 #  define API_PUBLIC __declspec(dllimport)
 # endif
 #elif defined __OS2__
-# ifdef BUILDING_LIBXDGDESKTOPENTRY
+# ifdef BUILDING_LIBXDGPP
 #  define API_PUBLIC __declspec(dllexport)
 # else
 #  define API_PUBLIC
 # endif
 #else
-# ifdef BUILDING_LIBXDGDESKTOPENTRY
+# ifdef BUILDING_LIBXDGPP
 #  define API_PUBLIC __attribute__((visibility("default")))
 # else
 #  define API_PUBLIC
